dddb6686c6c3e498c0587bfa3c7afdc4b6bbb7e3
MalcolmorianVII <bdkutambe@gmail.com>
2021-09-09 10:45:42 +0200

    JI of normalized results

--- napa/scripts/JacardCalc.R
+++ napa/scripts/JacardCalc.R
@@ -4,7 +4,7 @@
 # B = g5 unique
 # | A ∩ B| = g3g5 column
 
-#library(readxl);library(dplyr);library(ggplot2)
+library(readxl);library(dplyr);library(ggplot2)
 JI <- function(g3,g5,g3g5) {
   union =  (g3 + g5 ) - g3g5
   J = g3g5/union
@@ -13,18 +13,19 @@ JI <- function(g3,g5,g3g5) {
 
 
 # Data path
-vcfsIntersectIntersect <- read_excel("/Users/malcolmorian/Documents/Transfer/Docs/SNR-SARS-Cov/Guppy3Guppy5/2021.08.17_NAPA/2021.09.06.vcfIntersect.xlsx")
-#View(vcfsIntersect)
+#vcfsIntersect <- read_excel("/Users/malcolmorian/Documents/Transfer/Docs/SNR-SARS-Cov/Guppy3Guppy5/2021.08.17_NAPA/2021.09.06.vcfIntersect.xlsx")
+vcfsnormIntersect <- read_excel("/Users/malcolmorian/Documents/Transfer/Docs/SNR-SARS-Cov/Guppy3Guppy5/2021.08.17_NAPA/2021.09.09.vcfnormIntersect.xlsx")
+View(vcfsnormIntersect)
 
 # Calculating JI in vcfIntersect
-JIAllSpecies <- JI(vcfsIntersect$g3uniq,vcfsIntersect$g5uniq,vcfsIntersect$g3g5intersect)
+JIAllSpecies <- JI(vcfsnormIntersect$g3uniq,vcfsnormIntersect$g5uniq,vcfsnormIntersect$g3g5intersect)
 #JIAllSpecies
 
-vcfsIntersect$JIndex <- JIAllSpecies  # Adding the JI
+vcfsnormIntersect$JIndex <- JIAllSpecies  # Adding the JI
 
 
-View(vcfsIntersect)
+View(vcfsnormIntersect)
 
-JI_mean <- mean(vcfsIntersect$JIndex)
-JI_median <- median(vcfsIntersect$JIndex)
-
+JI_mean <- mean(vcfsnormIntersect$JIndex)
+JI_median <- median(vcfsnormIntersect$JIndex)
+JI_median
