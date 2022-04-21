567ea0cdc043e9fa2f5a14481fa099591c2e8bb9
MalcolmorianVII <bdkutambe@gmail.com>
2022-04-21 13:50:45 +0200

    Removed flye bash script

--- scripts/flye.sh
+++
@@ -1,8 +0,0 @@
-#! /usr/bin/bash
-
-#NOTE:  $1 = {wildcards.ref}; $2 =  {wildcards.results} $3 = model
-set -e
-eval "$(conda shell.bash hook)"
-conda activate "flye"
-
-mkdir $1 && flye --nano-hq $2 -g 5m -o $1 -t 8 --plasmids
